a9c2ba0fae95e4882447070881b96319eb6403d0
Rafał Świerzyński <rafal.swierzynski@jambit.com>
2020-11-10 23:20:18 +0100

    Add BLoC to the mix - functional widget broken

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MaterialApp(home: App()));
@@ -10,55 +12,49 @@ class App extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Builder(
-              builder: (subContext) => ElevatedButton(
-                onPressed: () => _showSnackBar(subContext, 'Inline'),
+        child: BlocProvider(
+          create: (context) => BlahBloc(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                onPressed: () => _showSnackBar(context, 'Inline'),
                 child: Text('Inline'),
               ),
-            ),
-            _createButton(),
-            RemoteCamerasSnackBarShowingElevatedButtonWhichExistsOnlyToGetABuildContext(
-              context,
-            ),
-          ],
+              RemoteCamerasSnackBarShowingElevatedButtonWhichExistsOnlyToGetABuildContext(),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-Widget _createButton() {
-  return Builder(
-    builder: (subContext) => ElevatedButton(
-      onPressed: () => _showSnackBar(subContext, 'Functional'),
-      child: Text('Functional'),
-    ),
-  );
-}
-
 class RemoteCamerasSnackBarShowingElevatedButtonWhichExistsOnlyToGetABuildContext
     extends StatelessWidget {
-  final BuildContext parentContext;
-
-  const RemoteCamerasSnackBarShowingElevatedButtonWhichExistsOnlyToGetABuildContext(
-    this.parentContext,
-  );
-
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () => _showSnackBar(parentContext, 'Widget'),
+      onPressed: () => _showSnackBar(context, 'Widget'),
       child: Text('Widget'),
     );
   }
 }
 
 void _showSnackBar(BuildContext context, String text) {
+  final state = BlocProvider.of<BlahBloc>(context).state;
   final scaffoldState = Scaffold.of(context);
   scaffoldState.hideCurrentSnackBar();
-  scaffoldState.showSnackBar(SnackBar(content: Text(text)));
+  scaffoldState.showSnackBar(SnackBar(content: Text('$text $state')));
+}
+
+// ------------------------------
+
+class BlahBloc extends Bloc<void, String> {
+  @override
+  String get initialState => 'BLoC found';
+
+  @override
+  Stream<String> mapEventToState(void _) async* {}
 }
