ee989aa78902c5938ba9fd69d2489c348699021d
Rafał Świerzyński <rafal.swierzynski@jambit.com>
2020-11-20 23:30:26 +0100

    Break BLoC by swapping widgets
    Tree structure changes so the BlocProvider loses its state, it is just a
    normal widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(App());
@@ -13,35 +12,46 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showCounterFirst;
+
+  @override
+  void initState() {
+    super.initState();
+    showCounterFirst = true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    print('### _AppState.build()');
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           actions: [
             IconButton(
-              icon: Icon(Icons.refresh),
+              icon: Icon(Icons.swap_vert),
               onPressed: () {
-                setState(() {});
+                setState(() {
+                  showCounterFirst = !showCounterFirst;
+                });
               },
             ),
           ],
         ),
-        body: BlocProvider(
-          create: (context) => CounterBloc(),
-          child: BlocProvider(
-            create: (context) => DummyBlocA(
-              BlocProvider.of<CounterBloc>(context),
-            ),
-            child: BlocProvider(
-              create: (context) => DummyBlocB(
-                BlocProvider.of<DummyBlocA>(context),
-              ),
-              child: CounterWidget(),
-            ),
-          ),
+        body: Column(
+          children: showCounterFirst
+              ? [
+                  BlocProvider(
+                    create: (context) => CounterBloc(),
+                    child: CounterWidget(),
+                  ),
+                  Text('Blah', style: Theme.of(context).textTheme.headline4),
+                ]
+              : [
+                  Text('Blah', style: Theme.of(context).textTheme.headline4),
+                  BlocProvider(
+                    create: (context) => CounterBloc(),
+                    child: CounterWidget(),
+                  ),
+                ],
         ),
       ),
     );
@@ -76,63 +86,14 @@ class CounterWidget extends StatelessWidget {
 
 // CounterBloc
 
-class LoggingCounterBlocProvider extends BlocProvider<CounterBloc> {
-  LoggingCounterBlocProvider({ValueBuilder<CounterBloc> create, Widget child})
-      : super(create: create, child: child) {
-    print('### LoggingCounterBlocProvider()');
-  }
-
-  LoggingCounterBlocProvider.value({CounterBloc value, Widget child})
-      : super.value(value: value, child: child) {
-    print('### LoggingCounterBlocProvider.value()');
-  }
-}
-
 class IncrementedEvent {}
 
 class CounterBloc extends Bloc<IncrementedEvent, int> {
-  CounterBloc() {
-    print('### $hashCode CounterBloc()');
-  }
-
   @override
   int get initialState => 0;
 
   @override
   Stream<int> mapEventToState(IncrementedEvent event) async* {
-    print('### $hashCode CounterBloc.mapEventToState(${event.runtimeType})');
     yield state + 1;
   }
-}
-
-// DummyBlocA
-
-class DummyBlocA extends Bloc<void, void> {
-  final CounterBloc counterBloc;
-
-  DummyBlocA(this.counterBloc) {
-    print('### DummyBlocA()');
-  }
-
-  @override
-  void get initialState => null;
-
-  @override
-  Stream<void> mapEventToState(void event) async* {}
-}
-
-// DummyBlocB
-
-class DummyBlocB extends Bloc<void, void> {
-  final DummyBlocA blocA;
-
-  DummyBlocB(this.blocA) {
-    print('### DummyBlocB()');
-  }
-
-  @override
-  void get initialState => null;
-
-  @override
-  Stream<void> mapEventToState(void event) async* {}
 }
